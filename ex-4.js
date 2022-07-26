63c81a420650e28a651df5d98a06c7b3ee043e0e
Kant <98311953+Kladex@users.noreply.github.com>
2022-07-26 17:52:29 +0700

    Update ex-4.js

--- ex-4.js
+++ ex-4.js
@@ -1,21 +1,4 @@
-// Start coding here
-
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
+// Exercise #4 Grouping Function with Objects
 
 // Start coding here
-
 let calculator = {};
