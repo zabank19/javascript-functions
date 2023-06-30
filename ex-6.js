08081e3908a59dd4f05d66db76080ae6c5454ccf
Mu <muvonglao@gmail.com>
2023-06-30 14:44:17 +0700

    added debugging ex

---
+++ ex-6.js
@@ -0,0 +1,8 @@
+//Exercise #6 Debugging (Array)
+
+//Start coding here
+const fruits = ["apple", "banana", "orange"];
+
+console.log("First Fruit: " + fruits[1]);
+console.log("Second Fruit: " + fruit[2]);
+console.log("Third Fruit: " + fruits[3]);
